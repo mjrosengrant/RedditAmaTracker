6f764c7d6242c022d68a857cd0045bced41b5e89
Mike Rosengrant <mjrosengrant@yahoo.com>
2014-05-26 23:53:56 -0700

    searchQueryLinkText()
    Added searchQueryLinkText() function

--- js/RedditController.js
+++ js/RedditController.js
@@ -5,12 +5,10 @@ myApp.controller('RedditController', ['$scope', function($scope){
     $scope.searchResults;//JSON object that holds search results
     
     $scope.searchAmas = function(){
-        //Example URL: http://www.reddit.com/r/IAmA/comments/1ri1y9/i_am_richard_dawkins_scientist_researcher_author/
-        
         var keywords = $scope.searchTerms;
-        $scope.searchTerms = '';
         var xmlHttp = null;
         var startOfUrl = 'http://www.reddit.com/r/IamA/search.json?q=';
+        
         //When placed in the url this limits search terms to specific subreddit and sorts results by relevance
         var endOfUrl = '&sort=relevance&restrict_sr=on';
         
@@ -28,6 +26,21 @@ myApp.controller('RedditController', ['$scope', function($scope){
     
     $scope.setSearchResults = function (){
         $scope.searchResults = JSON.parse($scope.searchAmas());
+    }
+    
+    //Makes the link that the main HTML page prints in Search Results.
+    //Current format is "03/17/2014- Title of AMA (Upvote Count)"
+    $scope.searchQueryLinkText = function (index){
+        $scope.state= "searchQueryStarted";
+        var utc   = $scope.searchResults.data.children[index].data.created_utc;
+        var title = $scope.searchResults.data.children[index].data.title;
+        var score = $scope.searchResults.data.children[index].data.score;
+        
+        var date = new Date(utc*1000);
+        date = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear();
+        $scope.state = utc;
+        
+        return date +"- " + title + " (" + score + ")";
     }
     
 }]);
